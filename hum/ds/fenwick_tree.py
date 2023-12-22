8d3c4926985e59cedef62f184ebfa5d9401e6bbb
humwawe <wawe8963098@163.com>
2023-12-22 23:03:39 +0800

    fenwick_tree

--- hum/ds/fenwick_tree.py
+++ hum/ds/fenwick_tree.py
@@ -3,7 +3,10 @@ class FenwickTree:
     self.n = n
     self.bit = [0] * n
 
-  def get_sum(self, r):
+  def get_sum(self, l=0, r=0):
+    return self.__get_sum(r) - self.__get_sum(l - 1)
+
+  def __get_sum(self, r):
     res = 0
     while r >= 0:
       res += self.bit[r]
@@ -15,11 +18,8 @@ class FenwickTree:
       self.bit[idx] += v
       idx = idx | (idx + 1)
 
-  def get_range_sum(self, l, r):
-    return self.get_sum(r) - self.get_sum(l - 1)
-
   def get(self, idx):
-    return self.get_range_sum(idx, idx)
+    return self.get_sum(idx, idx)
 
   def set(self, idx, v):
     self.add(idx, v - self.get(idx))
