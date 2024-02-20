23bb629e088f6c1ae1f97324e900ce588fcde251
humwawe <wawe8963098@163.com>
2024-02-20 23:14:39 +0800

    StringHash

--- hum/strs/string_hash.py
+++ hum/strs/string_hash.py
@@ -3,12 +3,12 @@ from random import random
 
 class StringHash:
   def __init__(self, s):
-    self.n = len(s)
+    self.N = len(s)
     self.BASE = 131  # random.randint(100, 500)
     self.MOD = random.getrandbits(64)
-    self.h = h = [0] * (self.n + 1)
-    self.p = p = [1] * (self.n + 1)
-    for i in range(1, self.n + 1):
+    self.h = h = [0] * (self.N + 1)
+    self.p = p = [1] * (self.N + 1)
+    for i in range(1, self.N + 1):
       p[i] = (p[i - 1] * self.BASE) % self.MOD
       h[i] = (h[i - 1] * self.BASE + ord(s[i - 1])) % self.MOD
 
@@ -24,5 +24,5 @@ class StringHash:
       if l is None:
         l = 0
       if r is None:
-        r = self.n
+        r = self.N
       return self.__get_hash(l, r)
